f8fbafde179510009b877a87bb8b80b3e9bf2a4a
Lucas Teixeira <lucas.at.negocios@gmail.com>
2020-05-22 19:25:00 -0300

    adding database

---
+++ src/app/services/CreateAppointmentService.ts
@@ -0,0 +1,36 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentRepository';
+
+interface RequestDTO {
+  date: Date;
+  provider: string;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameHour = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameHour) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
